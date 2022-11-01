9056e9d5ca9aad7f7cec42be94aec7947b038d9e
Malihe <m.moradi.ps@gmail.com>
2022-11-01 14:47:20 +0330

    Clean the location service

--- app/src/main/java/ir/malihemoradi/mapapplication/helper/ForegroundOnlyLocationService.kt
+++ app/src/main/java/ir/malihemoradi/mapapplication/helper/ForegroundOnlyLocationService.kt
@@ -1,25 +1,9 @@
-/*
- * Copyright 2019 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package ir.malihemoradi.mapapplication.helper
 
 import android.annotation.SuppressLint
-import android.app.NotificationManager
 import android.app.Service
-import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.location.Location
 import android.os.Binder
 import android.os.IBinder
@@ -29,7 +13,6 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.google.android.gms.location.*
 import java.util.concurrent.TimeUnit
 
-
 class ForegroundOnlyLocationService : Service() {
 
     private var configurationChange = false
@@ -37,8 +20,6 @@ class ForegroundOnlyLocationService : Service() {
     private var serviceRunningInForeground = false
 
     private val localBinder = LocalBinder()
-
-    private lateinit var notificationManager: NotificationManager
 
     // TODO: Step 1.1, Review variables (no changes).
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
@@ -50,9 +31,6 @@ class ForegroundOnlyLocationService : Service() {
 
     @SuppressLint("MissingPermission")
     override fun onCreate() {
-
-        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
 
         // TODO: Step 1.2, Review the FusedLocationProviderClient.
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
@@ -100,13 +78,6 @@ class ForegroundOnlyLocationService : Service() {
                 intent.putExtra(EXTRA_LOCATION, currentLocation)
                 LocalBroadcastManager.getInstance(applicationContext).sendBroadcast(intent)
 
-                // Updates notification content if this service is running as a foreground
-                // service.
-                if (serviceRunningInForeground) {
-//                    notificationManager.notify(
-//                        NOTIFICATION_ID,
-//                        generateNotification(currentLocation))
-                }
             }
         }
         fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null)
@@ -172,117 +143,26 @@ class ForegroundOnlyLocationService : Service() {
         }
     }
 
-//    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-//        val cancelLocationTrackingFromNotification = intent.getBooleanExtra(EXTRA_CANCEL_LOCATION_TRACKING_FROM_NOTIFICATION, false)
-//
-//        if (cancelLocationTrackingFromNotification) {
-////            unsubscribeToLocationUpdates()
-//            stopSelf()
-//        }
-//        return START_NOT_STICKY
-//    }
 
     override fun onBind(intent: Intent): IBinder {
-//        stopForeground(true)
-//        serviceRunningInForeground = false
-//        configurationChange = false
+        stopForeground(true)
+        serviceRunningInForeground = false
+        configurationChange = false
         return localBinder
     }
 
-//    override fun onRebind(intent: Intent) {
-////        stopForeground(true)
-////        serviceRunningInForeground = false
-////        configurationChange = false
-//        super.onRebind(intent)
-//    }
+    override fun onRebind(intent: Intent) {
+        stopForeground(true)
+        serviceRunningInForeground = false
+        configurationChange = false
+        super.onRebind(intent)
+    }
 
-//    override fun onUnbind(intent: Intent): Boolean {
-//        if (!configurationChange && SharedPreferenceUtil.getLocationTrackingPref(this)) {
-//            Log.d(TAG, "Start foreground service")
-//            val notification = generateNotification(currentLocation)
-//            startForeground(NOTIFICATION_ID, notification)
-//            serviceRunningInForeground = true
-//        }
-//
-//        // Ensures onRebind() is called if MainActivity (client) rebinds.
-//        return true
-//    }
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        configurationChange = true
+    }
 
-//    override fun onConfigurationChanged(newConfig: Configuration) {
-//        super.onConfigurationChanged(newConfig)
-//        configurationChange = true
-//    }
-
-    /*
-     * Generates a BIG_TEXT_STYLE Notification that represent latest location.
-     */
-//    private fun generateNotification(location: Location?): Notification {
-//        Log.d(TAG, "generateNotification()")
-//
-//        // Main steps for building a BIG_TEXT_STYLE notification:
-//        //      0. Get data
-//        //      1. Create Notification Channel for O+
-//        //      2. Build the BIG_TEXT_STYLE
-//        //      3. Set up Intent / Pending Intent for notification
-//        //      4. Build and issue the notification
-//
-//        // 0. Get data
-//        val mainNotificationText = location?.toText() ?: getString(R.string.no_location_text)
-//        val titleText = getString(R.string.app_name)
-//
-//        // 1. Create Notification Channel for O+ and beyond devices (26+).
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//
-//            val notificationChannel = NotificationChannel(
-//                NOTIFICATION_CHANNEL_ID, titleText, NotificationManager.IMPORTANCE_DEFAULT)
-//
-//            // Adds NotificationChannel to system. Attempting to create an
-//            // existing notification channel with its original values performs
-//            // no operation, so it's safe to perform the below sequence.
-//            notificationManager.createNotificationChannel(notificationChannel)
-//        }
-//
-//        // 2. Build the BIG_TEXT_STYLE.
-//        val bigTextStyle = NotificationCompat.BigTextStyle()
-//            .bigText(mainNotificationText)
-//            .setBigContentTitle(titleText)
-//
-//        // 3. Set up main Intent/Pending Intents for notification.
-//        val launchActivityIntent = Intent(this, MainActivity::class.java)
-//
-//        val cancelIntent = Intent(this, ForegroundOnlyLocationService::class.java)
-//        cancelIntent.putExtra(EXTRA_CANCEL_LOCATION_TRACKING_FROM_NOTIFICATION, true)
-//
-//        val servicePendingIntent = PendingIntent.getService(
-//            this, 0, cancelIntent, PendingIntent.FLAG_UPDATE_CURRENT)
-//
-//        val activityPendingIntent = PendingIntent.getActivity(
-//            this, 0, launchActivityIntent, 0)
-//
-//        // 4. Build and issue the notification.
-//        // Notification Channel Id is ignored for Android pre O (26).
-//        val notificationCompatBuilder =
-//            NotificationCompat.Builder(applicationContext, NOTIFICATION_CHANNEL_ID)
-//
-//        return notificationCompatBuilder
-//            .setStyle(bigTextStyle)
-//            .setContentTitle(titleText)
-//            .setContentText(mainNotificationText)
-//            .setSmallIcon(R.mipmap.ic_launcher)
-//            .setDefaults(NotificationCompat.DEFAULT_ALL)
-//            .setOngoing(true)
-//            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-////            .addAction(
-////                R.drawable.ic_launch, getString(R.string.launch_activity),
-////                activityPendingIntent
-////            )
-////            .addAction(
-////                R.drawable.ic_cancel,
-////                getString(R.string.stop_location_updates_button_text),
-////                servicePendingIntent
-////            )
-//            .build()
-//    }
 
     /**
      * Class used for the client Binder.  Since this service runs in the same process as its
@@ -302,12 +182,5 @@ class ForegroundOnlyLocationService : Service() {
             "$PACKAGE_NAME.action.FOREGROUND_ONLY_LOCATION_BROADCAST"
 
         internal const val EXTRA_LOCATION = "$PACKAGE_NAME.extra.LOCATION"
-
-        private const val EXTRA_CANCEL_LOCATION_TRACKING_FROM_NOTIFICATION =
-            "$PACKAGE_NAME.extra.CANCEL_LOCATION_TRACKING_FROM_NOTIFICATION"
-
-        private const val NOTIFICATION_ID = 12345678
-
-        private const val NOTIFICATION_CHANNEL_ID = "while_in_use_channel_01"
     }
 }
